733b5370fd466019aa8363d8a0d28e70cb6b79e2
Evan L'HUISSIER <e.lhuissier@krok.ovh>
2021-12-03 05:06:38 +0100

    Formulaire ajout sauvetage
    ça marche pas évidemment mais YOLO

--- view/single/cartes.php
+++ view/single/cartes.php
@@ -14,9 +14,39 @@
    
     </head>
     <body>
-        <div id="map-container">
-        <div id="map" style="height: 400px; width: 700px;"></div>
-        </div>
+    <div id="map-form-container">
+        <div id="map" style="height: 50vh; width: 80vh;"></div>
+
+        <div id="form-container">
+                <form action="action" method="post">
+                    <p>
+                        <label for="lat">Latitude</label>
+                        <input type="text" name="lat" id="lat" value="{{$lat}}">
+                    </p>
+                    <p>
+                        <label for="lng">Longitude</label>
+                        <input type="text" name="lng" id="lng" value="{{$lng}}">
+                    </p>
+                    <p>
+                        <label for="zoom">Zoom</label>
+                        <input type="text" name="zoom" id="zoom" value="{{$zoom}}">
+                    </p>
+                    <p>
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" id="nom" value="{{$nom}}">
+                    </p>
+                    <p>
+                        <label for="description">Description</label>
+                        <input type="text" name="description" id="description" value="{{$description}}">
+                    </p>
+                    <p>
+                        <label for="adresse">Adresse</label>
+                        <input type="text" name="adresse" id="adresse" value="{{$adresse}}">
+                    </p>
+                    <input type="submit" value="Envoyer">
+                </form>
+            </div>
+    </div>
     </body>
 
     <script src="js/carte.js"></script>
