3405d9d5731096f6664a516431928381ed373a5d
hug0be <83722580+hug0be@users.noreply.github.com>
2021-12-03 06:29:50 +0100

    Changed map form

--- view/all/sauvetages.php
+++ view/all/sauvetages.php
@@ -16,20 +16,27 @@
         $cmdt = Sauveteur::one($sauvetage->id_commandant);
         $cmdt = Personne::one($cmdt->id_personne);
         $sous_cmdt = Sauveteur::one($sauvetage->id_sous_commandant);
-        $sous_cmdt = Sauveteur::one($sous_cmdt->id_personne);
+        $sous_cmdt = Personne::one($sous_cmdt->id_personne);
         echo "<h2>Sauvetage du ".$sauvetage->date_sauvetage."</h2>";
         echo "<div style='margin:30px 0px' class='column'>";
         echo "<div class='glass slider round'></div>";
         echo "<div class='glass slider_img centered round blue'>Pas d'images :(</div>";
         echo "</div>";
-        echo "
-        <div class='column'>
-          <div class='sauvetage_photo'></div>
+        echo "<div class='column'>
+          <div class='glass centered sauvetage_photo'>Pas de photo :(</div>
           <div class='sauvetage_desc'>
             <p>Le capitaine :</p>
             <a class='underlined'>en savoir +</a>
           </div>
-          <h2 class='sauvetage_name'>{$cmdt->nom} PUTEUH</h2>
+          <h2 class='sauvetage_name'>{$cmdt->nom}, {$cmdt->prenom}</h2>
+        </div>";
+        echo "<div class='column'>
+          <div class='glass centered sauvetage_photo'>Pas de photo :(</div>
+          <div class='sauvetage_desc'>
+            <p>Le sous-capitaine :</p>
+            <a class='underlined'>en savoir +</a>
+          </div>
+          <h2 class='sauvetage_name'>{$sous_cmdt->nom}, {$sous_cmdt->prenom}</h2>
         </div>";
       }
       ?>
