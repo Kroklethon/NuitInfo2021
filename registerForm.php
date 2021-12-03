49ecff8a6ec760437b467b02cc1563f727b5d7f4
Kiki066 <Violet.k46@gmail.com>
2021-12-03 02:30:12 +0100

    Log & Co

---
+++ registerForm.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div id="id_logForm" class="id_userIdentification">
+            <h1>S'inscrire</h1>
+
+            <form method="post" action="">
+                <p><input type="text" name="nom_util" placeholder="Nom"> <input type="text" name="prenom_util" placeholder="Prénom"></p> 
+                <p><input type="email" name="mail_util"  placeholder="Adresse mail" required></p>
+                <p><input type="password" name="mdp_util"  placeholder="Mot de passe" required></p>
+                <p><input type="submit" value="Inscription" name="log-in" id="log-in"><a href="index.php">Déjà inscrit?</a></p>
+            </form>
+        </div>
+</body>
+</html>
