481f520a8f3029c296b11bf24794299363023365
hug0be <83722580+hug0be@users.noreply.github.com>
2021-12-03 03:17:53 +0100

    Added models

---
+++ models/Survie.php
@@ -0,0 +1,16 @@
+<?php
+
+class Survie extends Model {
+	// public function save() {
+	// 	$stm = db()->prepare("
+	// 		update Sauvetage
+	// 		set name=:name,
+	// 		    country=:country
+	// 		where idbrasserie=:idbrasserie
+	// 		");
+	// 	$stm->bindValue(":name", $this->name);
+	// 	$stm->bindValue(":country", $this->country);
+	// 	$stm->bindValue(":idbrasserie", $this->idbrasserie);
+	// 	$stm->execute();
+	// }
+}
